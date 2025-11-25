a34ba90c23f0899f98fe64609263efef65320b32
Dishen-Gajera <dishengajera@gmail.com>
2025-11-25 11:04:01 +0530

    change server url

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./Redux/store";
-export const serverUrl = "http://localhost:8000";
+export const serverUrl = "https://chatapk-backend.onrender.com";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
